4d3a016caa8fadd38d19a20fa7c0e74e3312aa26
BillWang3760 <2400011002@stu.pku.edu.cn>
2026-06-04 16:06:42 +0800

    Add files via upload

--- src/VCX/Labs/4-RBxFLIP/App.h
+++ src/VCX/Labs/4-RBxFLIP/App.h
@@ -5,16 +5,19 @@
 #include "Engine/app.h"
 #include "Labs/Common/UI.h"
 #include "CaseRBxFLIP.h"
+#include "CaseFEMxFLIP.h"
 
 namespace VCX::Labs::RBxFLIP {
     class App : public VCX::Engine::IApp {
     private:
         Common::UI      _ui;
         CaseRBxFLIP   _caseRBxFLIP;
+        CaseFEMxFLIP  _caseFEMxFLIP;
         std::size_t _caseId = 0;
 
         std::vector<std::reference_wrapper<Common::ICase>> _cases = {
-            _caseRBxFLIP
+            _caseRBxFLIP,
+            _caseFEMxFLIP
         };
 
     public:
